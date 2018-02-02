4abb001aeebb0bd4c842683e080f3a4a5f60a024
roilev <mrleroi@gmail.com>
2018-02-02 15:18:18 -0500

    Added Unity files

---
+++ Assets/Class1/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed = 0.1f;  
+
+	public KeyCode rightKey = KeyCode.RightArrow;
+	public KeyCode leftKey = KeyCode.LeftArrow;
+
+	public Sprite player1;
+	public Sprite player2;
+
+	public float bottomOfScreen = -5f;
+
+	public string sceneToSwitchToOnDeath = "GameOverScene";
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		
+		//Choose player
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			Debug.Log ("Changing Player");
+
+			if (this.GetComponent<SpriteRenderer> ().sprite == player1) {
+				this.GetComponent<SpriteRenderer> ().sprite = player2;
+			} else if (this.GetComponent<SpriteRenderer> ().sprite == player2) {
+				this.GetComponent<SpriteRenderer> ().sprite = player1;
+			}
+		}
+
+		//Movements
+		if (Input.GetKey(rightKey)) {
+			transform.Translate(moveSpeed, 0, 0);
+		}
+		if (Input.GetKey(leftKey)) {
+			transform.Translate(-moveSpeed, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			transform.Translate (0, moveSpeed, 0); 
+
+		} else if (Input.GetKeyUp (KeyCode.Space)) {
+			transform.Translate (0, -moveSpeed, 0);
+		}
+
+//		if (transform.position.y < bottomOfScreen) {
+//			SceneManager.LoadScene(sceneToSwitchToOnDeath);
+//		}
+	}
+}
---
+++ Assets/buttonB.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class buttonB : MonoBehaviour {
+
+	public GameObject character1;
+	public GameObject character2;
+	public GameObject character3;
+	public GameObject character4;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+	
+	void TaskOnClick()
+	{
+		Debug.Log("Changing character");
+		if (character1.activeSelf == true) {
+			character1.SetActive (false);
+			character2.SetActive (true);
+
+		} else if (character2.activeSelf == true) {
+			character2.SetActive (false);
+			character3.SetActive (true);
+
+		} else if (character3.activeSelf == true) {
+			character3.SetActive (false);
+			character4.SetActive (true);
+
+		} else if (character4.activeSelf == true) {
+			character4.SetActive (false);
+			character1.SetActive (true);
+		}
+	}
+}
